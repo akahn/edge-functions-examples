471edcb9c665d34e8b2a7123da7dbbbf3186dd04
Alex Kahn <alex.kahn@netlify.com>
2022-11-16 15:34:04 -0500

    Prefix newline too

--- netlify/edge-functions/log.ts
+++ netlify/edge-functions/log.ts
@@ -3,7 +3,7 @@ import type { Context } from "https://edge.netlify.com";
 export default async (request: Request, context: Context) => {
   context.log("Hello from the logging service");
   context.log("This is the second line from the logging service");
-  context.log("This is a line\nwith some newlines\n");
+  context.log("\nThis is a line\nwith some newlines\n");
 
   return new Response("The request to this URL was logged", {
     headers: { "content-type": "text/html" },
